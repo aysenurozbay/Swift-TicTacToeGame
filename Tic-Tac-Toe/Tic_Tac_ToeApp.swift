44fd82c54d0584adbf721860344f49cfc28e3f8f
aysenurozbay <aysenurozbay1004@gmail.com>
2023-01-03 22:09:59 +0300

    Initial Commit

---
+++ Tic-Tac-Toe/Tic_Tac_ToeApp.swift
@@ -0,0 +1,17 @@
+//
+//  Tic_Tac_ToeApp.swift
+//  Tic-Tac-Toe
+//
+//  Created by Ayşenur ÖZBAY on 3.01.2023.
+//
+
+import SwiftUI
+
+@main
+struct Tic_Tac_ToeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
